72b63320723d58a9e4b72f52f3415f27eea21871
Rohit Waghela <rohitwaghela321@gmail.com>
2017-05-10 18:56:59 +0530

    First version moved into github

---
+++ models/author.js
@@ -0,0 +1,33 @@
+var mongoose = require('mongoose');
+var moment = require('moment');
+var Schema = mongoose.Schema;
+
+var AuthorSchema = Schema({
+    first_name: { type: String, required: true, max: 100 },
+    family_name: { type: String, required: true, max: 100 },
+    date_of_birth: { type: Date },
+    date_of_death: { type: Date },
+});
+
+// Virtual for author's URL
+AuthorSchema
+    .virtual('url')
+    .get(function() {
+        return '/catalog/author/' + this._id;
+    });
+
+// Virtual for date format
+AuthorSchema
+    .virtual('date_of_birth_formatted')
+    .get(function() {
+        return this.date_of_birth ? moment(this.date_of_birth).format('YYYY-MM-DD') : '';
+    });
+
+AuthorSchema
+    .virtual('name')
+    .get(function() {
+        return this.first_name + " " + this.family_name;
+    });
+
+//Export model
+module.exports = mongoose.model('Author', AuthorSchema);
